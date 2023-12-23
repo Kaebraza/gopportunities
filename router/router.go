fef783ff79d7baf4526d99155cc3a0550982f592
Kaebraza <kaebraza@gmail.com>
2023-12-23 17:40:43 -0300

    feat: add routes logic

--- router/router.go
+++ router/router.go
@@ -2,18 +2,12 @@ package router
 
 import "github.com/gin-gonic/gin"
 
-// nome da função tem que ser maiúsculo para ser exportada
-func Initialize() { 
-	// incializa o router ultilizando as configs default do gin
+func Initialize() {
+	// Iniciallize router
 	router := gin.Default()
-	// go mod tidy limpa o go mod, instala o que falta e exclui o que não usa
-	// Define uma rota
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	// rodando a APIgo
-	router.Run("8080") // listen and serve on 0.0.0.0:8080
+	// Iniciallize routes
+	iniciallizeRoutes(router)
+	// Run server
+	router.Run("localhost:8080") // listen and serve on 0.0.0.0:8080
 
 }
