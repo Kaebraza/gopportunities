c6651d84915611019828bbf7f08dc2ee1820ed59
Kaebraza <kaebraza@gmail.com>
2023-12-24 09:21:35 -0300

    feat: Update handler structure to a separate subpackage

--- router/routes.go
+++ router/routes.go
@@ -1,43 +1,18 @@
 package router
 
 import (
-	"net/http"
-
+	Handler "github.com/Kaebraza/gopportunities/router/Handlers"
 	"github.com/gin-gonic/gin"
 )
 
 func iniciallizeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "GET Opening",
-			})
-		})
-
-		v1.POST("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "POST Opening",
-			})
-		})
-
-		v1.DELETE("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "DELETE Opening",
-			})
-		})
-
-		v1.PUT("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "PUT Opening",
-			})
-		})
-
-		v1.GET("/openings", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "GET Openings",
-			})
-		})
+		v1.GET("/opening", Handler.ShowOpeningHandler)
+		v1.POST("/opening", Handler.CreateOpeningHandler)
+		v1.DELETE("/opening", Handler.DeleteOpeningHandler)
+		v1.PUT("/opening", Handler.UpdateOpeningHandler)
+		v1.GET("/openings", Handler.ListOpeningsHandler)
 	}
 
 }
